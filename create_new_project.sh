e4020ff7d6238c00c48c2be54db38099695ff5af
laird-ikar <bguyot@student.42mulhouse.fr>
2021-12-21 02:48:16 +0100

    first (and maybe last) laziness.sh

---
+++ create_new_project.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:laird-ikar/tabula_rasa.git $1
+cd $1
+git init
+touch srcs/$1.c
+touch srcs/$1.h
+sed "s/PLACE_HOLDER/$1/" Makefile > /dev/null
---
+++ update_tabula_rasa.sh
@@ -0,0 +1,6 @@
+cp libft/libft.a tabula_rasa
+cp libft/libft.h tabula_rasa/srcs
+cd tabula_rasa
+git add *
+git commit -m "auto updated <3"
+git push gub
